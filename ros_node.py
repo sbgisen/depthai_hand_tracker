9df9f0c76206bd7348374af2a293bf6b8c68985a
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2023-06-30 17:08:50 +0900

    add rosnode

---
+++ ros_node.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+
+import rospy
+from cv_bridge import CvBridge
+from geometry_msgs.msg import Point
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+from HandTrackerEdge import HandTracker
+from HandTrackerRenderer import HandTrackerRenderer
+
+
+def gesture() -> None:
+
+    tracker = HandTracker(
+        use_gesture=True,
+        xyz=True,
+        stats=True,
+        # **tracker_args
+    )
+
+    renderer = HandTrackerRenderer(
+        tracker=tracker,
+    )
+
+    gesture_sign_pub = rospy.Publisher('gesture', String, queue_size=10)
+    image_pub = rospy.Publisher('detected_image', Image, queue_size=10)
+    palm_point_pub = rospy.Publisher('palm_point', Point, queue_size=10)
+    rospy.init_node('hand_tracking', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
+    while not rospy.is_shutdown():
+        # Run hand tracker on next frame
+        # 'bag' contains some information related to the frame
+        # and not related to a particular hand like body keypoints in Body Pre Focusing mode
+        # Currently 'bag' contains meaningful information only when Body Pre Focusing is used
+        # Draw hands
+        frame, hands, bag = tracker.next_frame()
+        if frame is not None:
+            frame = renderer.draw(frame, hands, bag)
+            image_pub.publish(CvBridge().cv2_to_imgmsg(frame, "bgr8"))
+        if len(hands) > 0:
+            gesture_sign = hands[0].gesture
+            rospy.loginfo(gesture_sign)
+            rospy.loginfo(hands[0].xyz[2])
+            palm_point = Point(hands[0].xyz[0], hands[0].xyz[1], hands[0].xyz[2])
+            gesture_sign_pub.publish(gesture_sign)
+            palm_point_pub.publish(palm_point)
+        rate.sleep()
+    renderer.exit()
+    tracker.exit()
+
+
+if __name__ == '__main__':
+    try:
+        gesture()
+    except rospy.ROSInterruptException:
+        pass
