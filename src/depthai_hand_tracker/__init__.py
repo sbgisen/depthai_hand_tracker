40f467faa243c19068c7dedf7a8e6ec840f27d06
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2023-07-01 10:00:24 +0900

    fix file path

--- src/depthai_hand_tracker/__init__.py
+++ src/depthai_hand_tracker/__init__.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-from depthai_hand_tracker import FPS
-from depthai_hand_tracker import HandTracker
-from depthai_hand_tracker import HandTrackerEdge
-from depthai_hand_tracker import HandTrackerRenderer
-from depthai_hand_tracker import mediapipe_utils
+from depthai_hand_tracker.FPS import FPS
+# from depthai_hand_tracker.HandTracker import HandTracker
+from depthai_hand_tracker.HandTrackerEdge import HandTracker
+from depthai_hand_tracker.HandTrackerRenderer import HandTrackerRenderer
 
 __all__ = [
     "HandTracker",
-    "HandTrackerEdge",
     "HandTrackerRenderer",
-    "mediapipe_utils",
     "FPS"
 ]
